e2b4208927fae5880be6380f5548d1cbc33f74ec
luchbheag <nedolivko.julia@gmail.com>
2024-06-19 18:50:01 +0200

    fix: check string with NumericUtils instead of StringUtils

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,14 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("11.1"));
+    }
+
     public static boolean isPositiveNumber(String str) {
-         return org.apache.commons.lang3.StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+         return NumberUtils.isCreatable(str) && Double.parseDouble(str) > 0;
     }
 }
